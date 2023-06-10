317849f6c865fc90fb1dc763db2ac8079b8dc4ae
SayhanA <sayhanahmed05@gmail.com>
2023-06-10 12:07:50 +0600

    add new class data added

--- src/Pages/Dashboard/AddClasses/AddClasses.jsx
+++ src/Pages/Dashboard/AddClasses/AddClasses.jsx
@@ -1,29 +1,70 @@
 import { useForm } from "react-hook-form";
 import useAuth from "../../../hooks/useAuth";
-
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import Swal from "sweetalert2";
+import { useState } from "react";
 
 const AddClasses = () => {
-    const { useImgHook } = useAuth();
-    
-    const { register, handleSubmit, formState: { errors } } = useForm();
+    const [axiosSecure] = useAxiosSecure();
+    const { useImgHook, user } = useAuth();
+    const [imgLoading, setImgLoading] = useState(false);
+
+    const { register, handleSubmit, reset } = useForm();
 
     const onSubmit = data => {
+        setImgLoading(true);
         data.status = 'pending';
+        data.email = user.email;
+        data.instructor = user.displayName;
         console.log(data);
 
         useImgHook(data)
-        .then(res => res.json())
-        .then(imgResponse => {
-            console.log(imgResponse)
-            if(imgResponse.success){
-                const imgURL = imgResponse.data.display_url;
-                console.log(imgURL)
-                const classData = data;
-                classData.sportImg = imgURL;
-                console.log(classData)
-            }
-        })
-        
+            .then(res => res.json())
+            .then(imgResponse => {
+                console.log(imgResponse)
+                if (imgResponse.success) {
+                    const imgURL = imgResponse.data.display_url;
+                    const classData = data;
+                    classData.sportImg = imgURL;
+                    console.log(classData)
+
+                    axiosSecure.post('/classes', classData)
+                        .then(data => {
+                            console.log("After posting new menu item", data.data)
+                            if (data.data.insertedId) {
+                                reset()
+                                setImgLoading(false)
+                                Swal.fire({
+                                    // position: 'top-end',
+                                    icon: 'success',
+                                    title: 'New class added',
+                                    showConfirmButton: false,
+                                    timer: 1500
+                                })
+                            }
+                            else {
+                                setImgLoading(false)
+                                Swal.fire({
+                                    icon: 'error',
+                                    title: 'Oops...',
+                                    text: 'Can not Inset Data!',
+                                    // footer: '<a href="">Why do I have this issue?</a>'
+                                })
+                            }
+                        })
+                }
+                else {
+                    setImgLoading(false)
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Oops...',
+                        text: 'Please Provide an Image ',
+                        // footer: '<a href="">Why do I have this issue?</a>'
+                    })
+                }
+            })
+
+
     };
 
     return (
@@ -31,16 +72,7 @@ const AddClasses = () => {
             <div className="bg-white rounded-xl p-20">
                 <h3 className="text-3xl text-center font-bold">Add a Class</h3>
                 <form onSubmit={handleSubmit(onSubmit)} className="pt-10 flex flex-col gap-5">
-                    <div className="grid md:grid-cols-2 md:gap-6">
-                        <div className="relative z-0 w-full mb-6 group">
-                            <input type="text" {...register("instructor")} name="instructor" id="instructor" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-                            <label htmlFor="instructor" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:-translate-y-6">Instructor name</label>
-                        </div>
-                        <div className="relative z-0 w-full mb-6 group">
-                            <input type="email" {...register("email")} name="email" id="email" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-                            <label htmlFor="email" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:-translate-y-6">Email address</label>
-                        </div>
-                    </div>
+
                     <div className="grid md:grid-cols-2 md:gap-6">
                         <div className="relative z-0 w-full mb-6 group">
                             <input type="text" {...register("sport")} name="sport" id="sport" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
@@ -49,6 +81,16 @@ const AddClasses = () => {
                         <div className="relative z-0 w-full mb-6 group">
                             <input type="number" {...register("availableSeats")} name="availableSeats" id="availableSeats" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
                             <label htmlFor="availableSeats" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:-translate-y-6">Available seats</label>
+                        </div>
+                    </div>
+                    <div className="grid md:grid-cols-2 md:gap-6">
+                        <div className="relative z-0 w-full mb-6 group">
+                            <input type="text" {...register("name")} name="name" id="name" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                            <label htmlFor="name" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:-translate-y-6">Academy name</label>
+                        </div>
+                        <div className="relative z-0 w-full mb-6 group">
+                            <input type="number" {...register("duration")} name="duration" id="duration" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                            <label htmlFor="duration" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:-translate-y-6">Class duration</label>
                         </div>
                     </div>
                     <div className="grid md:grid-cols-2 md:gap-6">
@@ -76,7 +118,7 @@ const AddClasses = () => {
 
                     </div>
 
-                    <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-4 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 w-full">Submit</button>
+                    <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-4 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 w-full"> {imgLoading ? "loading ...." : "Add Class"} </button>
                 </form>
             </div>
         </div>
